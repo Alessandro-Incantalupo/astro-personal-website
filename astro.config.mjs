45e11611a83e3d0a4a183751c7ebbafe01a83046
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-07-04 09:03:15 +0200

    refactor: changed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alessandroincantalupo.com/",
+  site: "https://www.alessandroincantalupo.com",
   integrations: [tailwind(), sitemap()],
 });
